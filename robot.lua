1f5630af791072a5f6c368a3927601d3ea4a8392
Zardar <Zardar@users.noreply.github.com>
2022-03-14 18:06:53 +0500

    Update robot.lua

--- robot.lua
+++ robot.lua
@@ -1,1 +1,36 @@
 
+
+--robot with scanner on up side
+local w=require('os').sleep
+local r=require('robot')
+local inv=r.inventorySize()
+local t=os.time()
+local c=r.compareTo
+
+function wait()
+r.select(1)
+while true do
+w(1)
+if c(15) then return trade()
+else r.drop(3)
+end
+end
+
+function trade()
+ r.drop(0)
+ t=os.time()
+ while os.time()-t < 9000 do
+ w(0.2)
+ i=getStackInInternalSlot(1)
+ if i then r.drop(1) return work()
+ else end
+
+function work()
+while not getStackInInternalSlot(1) do
+w(1)
+end
+r.drop(3)
+return wait()
+end
+
+wait()
